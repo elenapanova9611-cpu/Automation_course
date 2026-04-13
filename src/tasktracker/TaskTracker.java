11834dd6472f6ae9806f482788c276455156db74
elenapanova9611-cpu <elena.panova.9611@gmail.com>
2026-04-13 09:46:03 +0200

    3rd_homework_without_reading_from_file (#3)
    * 3rd_homework_without_reading_from_file
    
    * fixes
    
    * fix for entering string for priority field
    
    * practice enum with actionMenu
    
    * read from file added

--- src/tasktracker/TaskTracker.java
+++ src/tasktracker/TaskTracker.java
@@ -1,20 +1,18 @@
 package tasktracker;
 
-/*
- Изначально было решение через просто массив размером на 10 элементов, но с ним работать не так удобно, как с
- динамическим ArrayList
- static Task[] tasks1 = new Task[10];
- static int taskCount = 0;
 
-*/
-
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TaskTracker {
-   static ArrayList<Task> tasks = new ArrayList<Task>();
+    static final String FILE_PATH = "/Users/alenap/tasks.txt";
+    static ArrayList<Task> tasks = new ArrayList<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
           /*
         1. Create task model
         2. Show the menu
@@ -25,19 +23,31 @@ public class TaskTracker {
         4. if input = 2 --> Print all tasks stored in array
             4.1 show menu again
         5. if input = 0 --> finish the program
+
+        1. Создать конструктор для enum
+            1a. Причесать строку, которую юзер вводит
+        2. Создать метод, который переводит значение enum в строку
+        3. Если юзер неверно ввел статус --> попросить снова
+        4. читаем строку и из строки делаем массив подстрок и из этого фоормируем объект
          */
 
+        readTasksFromFile();
         while (true) {
             showMenu();
-            int userInput = getUserInput();
-            if (userInput == 1) {
-                tasks.add(createTask());
-            } else if (userInput == 2) {
-                printAllTasks();
-            } else if (userInput == 0) {
-                break;
-            } else {
-                System.out.println("Wrong choice, please try again");
+            ActionMenu action = ActionMenu.fromInt(getUserInput());
+            switch (action) {
+                case CREATE:
+                    tasks.add(createTask());
+                    writeTasksToFile();
+                    break;
+                case DISPLAY:
+                    printAllTasks();
+                    break;
+                case EXIT:
+                    return;
+                case UNKNOWN:
+                    System.out.println("Invalid input. Please try again.");
+                    break;
             }
         }
     }
@@ -66,15 +76,75 @@ public class TaskTracker {
         String taskDescription = userInput.nextLine();
 
         System.out.println("3. Task priority");
-        int taskPriority = userInput.nextInt();
 
-        return new Task(taskName, taskDescription, taskPriority);
+        int taskPriority = 0;
+        do {
+            try {
+                taskPriority = userInput.nextInt();
+                userInput.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println("You should enter a number");
+                userInput.nextLine();
+            }
+        } while (taskPriority == 0);
+
+        Status taskStatus;
+        do {
+            System.out.println("4. Task status (Open, In Progress, Ready For Test, In Test, Closed)");
+            taskStatus = Status.makeFromUserInput(userInput.nextLine());
+        } while (taskStatus == null);
+
+        return new Task(taskName, taskDescription, taskPriority, taskStatus);
     }
 
     public static void printAllTasks() {
         for (Task task : tasks) {
-                System.out.printf("Info about the task: \nTitle: %s \nDescription: %s \nPriority: %d\n",
-                        task.getName(), task.getDescription(), task.getPriority());
+            System.out.printf(task.getInfo());
+        }
+    }
+
+    public static String getDataForFileWriter(Task task) {
+        return String.format("%s/%s/%d/%s", task.getName(), task.getDescription(), task.getPriority(),
+                task.getStatus());
+    }
+
+    public static void writeTasksToFile() throws IOException {
+        FileWriter fileWriter = new FileWriter(FILE_PATH, false);
+        for (Task task : tasks) {
+            fileWriter.write(getDataForFileWriter(task) + "\n");
+        }
+        fileWriter.close();
+    }
+
+    public static void readTasksFromFile() throws IOException {
+        File file = new File(FILE_PATH);
+        if (!file.exists()) {
+            return;
+        }
+
+        tasks.clear();
+
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.isBlank()) {
+                    continue;
+                }
+
+                String[] substrings = line.split("/");
+
+                String taskName = substrings[0];
+                String taskDescription = substrings[1];
+                int taskPriority = Integer.parseInt(substrings[2]);
+                Status status = Status.makeFromUserInput(substrings[3]);
+
+                if (status == null) {
+                    continue;
+                }
+
+                Task task = new Task(taskName, taskDescription, taskPriority, status);
+                tasks.add(task);
+            }
         }
     }
 }
